42048ed1c8b4dc08e6075cf702f13081bad72175
Chai Landau <clandau@cliquestudios.com>
2022-05-08 20:23:34 -0400

    Work: -- adds case studies
    WorkIndiv:
    -- conditionally renders button depending on screen width
    -- adds individual browser window color option
    
    Styling:
    -- adds responsive styles

--- components/Work/Work.js
+++ components/Work/Work.js
@@ -6,7 +6,27 @@ const Work = () => {
         <div className="work">
             <Title text="Recent Work" />
             <div className="wrapper">
-                <WorkIndiv title="Half Moon Tavern" button="Learn More" />
+                <WorkIndiv
+                    title="Half Moon Tavern"
+                    button="Learn More"
+                    img="/img/halfmoon.png"
+                    alt="Half Moon Tavern Website with drink recipes"
+                    color="#af9bcf"
+                />
+                <WorkIndiv
+                    title="Alana Norell"
+                    button="Learn More"
+                    img="/img/alananorell.jpg"
+                    alt="Alana Norell Website with photos of jewelry"
+                    color="#eddfbf"
+                />
+                <WorkIndiv
+                    title="Concierge Home Cooking"
+                    button="Learn More"
+                    img="/img/conciergehomecooking.jpg"
+                    alt="Concierge Home Cooking Website with services and food photos"
+                    color="#fadbb4"
+                />
             </div>
         </div>
     );
--- components/Work/WorkIndiv.js
+++ components/Work/WorkIndiv.js
@@ -1,15 +1,45 @@
 import Image from "next/image";
+import { useState, useEffect } from "react";
 
 const WorkIndiv = (props) => {
+    const [mobile, setMobile] = useState(false);
+
+    useEffect(() => {
+        const mobileBreakpoint = window.matchMedia("(max-width: 1199px)");
+
+        const rearrangeMobile = () => {
+            if (mobileBreakpoint.matches) {
+                setMobile(true);
+            } else {
+                setMobile(false);
+            }
+        };
+
+        window.addEventListener("load", () => {
+            rearrangeMobile();
+        });
+
+        window.addEventListener("resize", () => {
+            setTimeout(() => {
+                rearrangeMobile();
+            }, 200);
+        });
+    }, [mobile]);
+
+    const browserWindowStyle = {
+        borderTopColor: `${props.color}`,
+    };
+
     return (
         <div className="work-indiv">
-            <div className="header">
+            <div className="info">
                 <h3 className="title-sm">{props.title}</h3>
-                <button className="tag">{props.button}</button>
+                {!mobile ? <button className="tag">{props.button}</button> : null}
             </div>
-            <div className="image">
-                <Image src="/img/halfmoon.png" alt="alt" layout="fill" />
+            <div className="image" style={browserWindowStyle}>
+                <Image src={props.img} alt={props.alt} layout="fill" />
             </div>
+            {mobile ? <button className="tag">{props.button}</button> : null}
         </div>
     );
 };
