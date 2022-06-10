1e75f608add6b205e495a1500559e2e450c6363b
Chai Landau <clandau@cliquestudios.com>
2022-06-09 21:48:47 -0400

    adds intro text animation

--- components/Intro.js
+++ components/Intro.js
@@ -1,6 +1,51 @@
+import { useRef, useEffect } from "react";
+import dynamic from "next/dynamic";
+import { gsap } from "gsap/dist/gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import { SplitText } from "gsap/dist/SplitText";
+
+gsap.registerPlugin(SplitText, ScrollTrigger);
+
 const Intro = () => {
+    const ref = useRef(null);
+
+    useEffect(() => {
+        const introElement = ref.current;
+
+        const titles = introElement.querySelectorAll(".title-lg");
+
+        return () => {
+            window.addEventListener("load", function () {
+                titles.forEach((title) => {
+                    let childSplit = new SplitText(title, {
+                        type: "lines",
+                        linesClass: "split-child",
+                    });
+                    let parentSplit = new SplitText(title, {
+                        type: "lines",
+                        linesClass: "split-parent",
+                    });
+
+                    const textAnimation = gsap.timeline({
+                        onComplete: () => {
+                            parentSplit.revert();
+                            childSplit.revert();
+                        },
+                    });
+
+                    textAnimation.from(childSplit.lines, {
+                        duration: 1.5,
+                        yPercent: 100,
+                        ease: "power4",
+                        stagger: 0.25,
+                    });
+                });
+            });
+        };
+    }, []);
+
     return (
-        <div className="intro">
+        <div className="intro" ref={ref}>
             <div className="wrapper">
                 <span className="title-lg">Hi, I’m Chai.</span>
                 <span className="title-lg">
